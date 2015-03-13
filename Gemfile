7b68cc4a1a5c2c85f4593dd0170ae459a9cc9d8c
Stephan Risch <stephanrisch@gmx.de>
2015-03-13 17:50:31 +0100

    gems

--- Gemfile
+++ Gemfile
@@ -37,4 +37,13 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem "activerecord-postgresql-adapter"
+end
+
 
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,25 @@ module WebQuest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
+
+    configure :development do
+      set :database, "sqlite3:///database.db"
+    end
+
+    configure :production do
+      db = URI.parse(ENV['HEROKU_POSTGRESQL_NAVY_URL'] || 'pg:psql --app sleepy-eyrie-8959')
+
+      ActiveRecord::Base.establish_connection(
+        :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+        :host     => db.host,
+        :username => db.user,
+        :password => db.password,
+        :database => db.path[1..-1],
+        :encoding => 'utf8'
+      )
+    end
   end
+
+  
+
 end
